94594700b5d8c1b959f761d0103878690fd057c4
NishizawaShota <nishizawa@emol.jp>
2019-03-06 07:09:30 +0900

    Finish photo delete  func.

--- PhotoDeleteApp/SubViewController.swift
+++ PhotoDeleteApp/SubViewController.swift
@@ -7,20 +7,44 @@
 //
 
 import UIKit
+import Photos
 
 class SubViewController: UIViewController {
 
     
     @IBOutlet weak var imageView: UIImageView!
-    var selectedImage: UIImage!
+    
+    var selectedImage: PHAsset!
+    
+    var uiViewControllerDelegate: UIViewControllerDelegate!
+    
+    //画像を削除する
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        uiViewControllerDelegate.deletePhotos()
+        
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imageView.image = selectedImage
+        //画像を表示
+        let manager: PHImageManager = PHImageManager()
+        manager.requestImage(for: selectedImage,
+                             targetSize: CGSize(width: 70, height: 70),
+                             contentMode: .aspectFill,
+                             options: nil) { (image, info) -> Void in
+                                self.imageView.image = image
+        }
         // 画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
         imageView.contentMode = UIView.ContentMode.scaleAspectFit
     }
+    
+    
     
 
 }
--- PhotoDeleteApp/ViewController.swift
+++ PhotoDeleteApp/ViewController.swift
@@ -7,26 +7,59 @@
 //
 
 import UIKit
+import Photos
+import Intents
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate ,UICollectionViewDelegateFlowLayout {
+protocol UIViewControllerDelegate {
+    func deletePhotos()
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate ,UICollectionViewDelegateFlowLayout, UIViewControllerDelegate {
     
-    let photos = ["aomori", "atami", "fukuoka", "kanazawa", "kobe", "kyoto", "okinawa"]
+    var photoAssets = [PHAsset]()
     
-    var selectedImage: UIImage?
+    var selectedImage: PHAsset?
+    var removeInt: Int!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    //画像を全て取得する
+    private func getAllPhotosInfo() {
+        photoAssets = []
+        
+        // 画像をすべて取得
+        var assets: PHFetchResult = PHAsset.fetchAssets(with: .image, options: nil)
+        assets.enumerateObjects { (asset, index, stop) -> Void in
+            self.photoAssets.append(asset as PHAsset)
+            print("did append!")
+        }
+        print(photoAssets)
+    }
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         //コレクションビューから識別子「TestCell」のセルを取得する。
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TestCell", for: indexPath) as UICollectionViewCell
         
         let imageView = cell.contentView.viewWithTag(1) as! UIImageView
-        let cellImage = UIImage(named: photos[indexPath.row])
-        imageView.image = cellImage
+        
+        
+        //画像を表示
+        let manager: PHImageManager = PHImageManager()
+        manager.requestImage(for: photoAssets[indexPath.row],
+                             targetSize: CGSize(width: 70, height: 70),
+                             contentMode: .aspectFill,
+                             options: nil) { (image, info) -> Void in
+                                imageView.image = image
+        }
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+        return photoAssets.count
     }
     
     
@@ -49,8 +82,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        selectedImage = UIImage(named: photos[indexPath.row])
-        
+        selectedImage = photoAssets[indexPath.row]
+        removeInt = indexPath.row
         if selectedImage != nil {
             performSegue(withIdentifier: "goToSubVC", sender: self)
         }
@@ -61,14 +94,50 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             let subVC: SubViewController = (segue.destination as? SubViewController)!
             // SubViewController のselectedImgに選択された画像を設定する
             subVC.selectedImage = selectedImage
+            subVC.uiViewControllerDelegate = self
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func deletePhotos() {
+        
+        if selectedImage != nil {
+            PHPhotoLibrary.shared().performChanges({ () -> Void in
+                // 削除などの変更はこのblocks内でリクエストする
+                PHAssetChangeRequest.deleteAssets([self.selectedImage!] as NSFastEnumeration)
+                
+            }, completionHandler: { (success, error) -> Void in
+                
+                if (success) {
+                    print("success!")
+                    
+                    self.dismiss(animated: true, completion: {
+                        self.updateCollectionView()
+                    })
+                    
+                } else {
+                    print("error!")
+                }
+                
+            })
+        }
     }
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getAllPhotosInfo()
+    }
+    
+    
+    func updateCollectionView() {
+        
+        photoAssets.remove(at: removeInt)
+        print("remove PhotoAssets: \(photoAssets)")
+        collectionView.reloadData()
+    }
+    
 }
+
+
 
